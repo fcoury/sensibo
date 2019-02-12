7290966854726394194376a705a866c7565f7abe
Colin Harris <col.w.harris@gmail.com>
2019-02-12 09:00:50 +0800

    Initial Commit

---
+++ Sensibo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Sensibo
+//
+//  Created by Colin Harris on 12/2/19.
+//  Copyright © 2019 Colin Harris. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Sensibo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sensibo
+//
+//  Created by Colin Harris on 12/2/19.
+//  Copyright © 2019 Colin Harris. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
